019c6aee394e73c6a4a68b637804ff083c586fc7
Mettra <Mettra@users.noreply.github.com>
2020-04-17 12:51:33 -0700

    Fixed compile error on binding static functions with no parameters

--- il2cpp/binding_template_helpers.h
+++ il2cpp/binding_template_helpers.h
@@ -47,10 +47,21 @@ struct is_valid_return_type {
 	static const bool value = std::is_same_v<Ret, void> || is_optional<Ret>::value;
 };
 
+template<typename... Args>
+constexpr bool checkThisPtr() {
+	if constexpr(sizeof...(Args) < 2) {
+		return false;
+	}
+	else {
+		using tuple_type = std::tuple<Args...>;
+		return std::is_same_v<std::tuple_element_t<1, tuple_type>, ThisPtr>;
+	}
+}
+
 template<typename Ret, typename... Args>
 struct is_valid_function_type<std::function<Ret(Args...)>> {
 	using tuple_type = std::tuple<Args...>;
 	static const bool hasContext = std::is_same_v<std::tuple_element_t<0, tuple_type>, const MethodInvocationContext&>;
-	static const bool hasThisPtr = std::is_same_v<std::tuple_element_t<1, tuple_type>, ThisPtr>;
+	static const bool hasThisPtr = checkThisPtr<Args...>();
 	static const bool hasValidReturn = is_valid_return_type<Ret>::value;
 };
